9856abb520ef5fb1c3bcbda04bcd3fade3e71f01
abroccoli <acbrocco@syr.edu>
2015-03-26 09:27:19 -0400

    wrote method to find palindromes

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,1 +1,13 @@
-var palindrome = {};
+var palindrome = {
+  count: function(phrases){
+    var dromes = [];
+
+    phrases.forEach(function(phrase){
+      var phrasestr = phrase.replace(/\s/g, '');
+      if (phrasestr.toLowerCase() === phrasestr.split('').reverse().join('').toLowerCase()){
+        dromes.push(phrase);
+      }
+    });
+    return dromes.length;
+  }
+};
